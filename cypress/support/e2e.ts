f8a2d0017d55c6b038b375f6a9539e926658eaa8
ChTor <xristina.1xr@gmail.com>
2025-12-02 21:28:48 +0200

    fix: remove unused file

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,5 +1,3 @@
-import "./commands";
-
 // You can add global before/beforeEach hooks here
 Cypress.on("uncaught:exception", () => {
   // returning false here prevents Cypress from failing the test
